72b3f91803b6e517d1ad29079942aac5bf56a4ba
lucasNSF <nascimentomr10@gmail.com>
2023-10-05 11:28:02 -0300

    style: finalizing the application style

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 import { Pokemon } from 'src/app/models/Pokemon';
 import { PokeApiService } from 'src/app/services/poke-api/poke-api.service';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatIconModule, MatInputModule],
+  imports: [CommonModule, FormsModule, MatButtonModule],
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
