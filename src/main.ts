093523e23d3114b4e7495ac39024641d0d4aab1e
lucasNSF <nascimentomr10@gmail.com>
2023-10-04 21:59:03 -0300

    refactor: remove routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule), provideRouter([])],
+  providers: [importProvidersFrom(HttpClientModule)],
 }).catch((err) => console.error(err));
